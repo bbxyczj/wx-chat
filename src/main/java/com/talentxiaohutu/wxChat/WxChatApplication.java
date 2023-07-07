895185d6f24afd7d5673f67b5e4095b765746099
陈正健 <chenzhengjian@log56.com>
2023-07-07 17:47:48 +0800

    1.first commit

---
+++ src/main/java/com/talentxiaohutu/wxChat/WxChatApplication.java
@@ -0,0 +1,13 @@
+package com.talentxiaohutu.wxChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/talentxiaohutu/wxChat/WxChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentxiaohutu.wxChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
