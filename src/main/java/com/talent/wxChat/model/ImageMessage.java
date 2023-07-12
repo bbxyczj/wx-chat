b27e779fdbe9fe3bbec07f2460f1a91926f4b3d9
陈正健 <chenzhengjian@log56.com>
2023-07-12 10:43:00 +0800

    1.docker deploy

--- src/main/java/com/talent/wxChat/model/ImageMessage.java
+++ src/main/java/com/talent/wxChat/model/ImageMessage.java
@@ -1,11 +1,13 @@
 package com.talent.wxChat.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author xiefc
  */
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class ImageMessage extends BaseMessage {
     private Image Image;
 }
--- src/main/java/com/talent/wxChat/model/TextMessage.java
+++ src/main/java/com/talent/wxChat/model/TextMessage.java
@@ -1,11 +1,13 @@
 package com.talent.wxChat.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author xiefc
  */
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class TextMessage extends BaseMessage{
     /**
      * 消息类型
